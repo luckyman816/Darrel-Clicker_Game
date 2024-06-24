ec4e0fdbd56ed6504ae162d3011aacc9e53bd5db
Senior Coder <agilecoder2040@gmail.com>
2024-06-24 17:20:48 +0900

    d

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -2,10 +2,10 @@ import "./Loading.css";
 const Loading = () => {
   return (
     <div className="loader">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div style={{backgroundImage: "url(/image/1.png)", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}></div>
+      <div style={{backgroundImage: "url(/image/2.png)", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}></div>
+      <div style={{backgroundImage: "url(/image/3.png)", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}></div>
+      <div style={{backgroundImage: "url(/image/4.png)", backgroundSize: "cover", backgroundRepeat: "no-repeat"}}></div>
     </div>
   );
 };
