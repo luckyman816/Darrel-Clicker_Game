b07e0befec7fae5702457f0616a2a470642de61b
Senior Coder <agilecoder2040@gmail.com>
2024-06-24 18:18:22 +0900

    d

--- src/page/Boost.tsx
+++ src/page/Boost.tsx
@@ -56,6 +56,13 @@ export default function Boost() {
   const handleCloseLimitModal = () => {
     setIsLimitModalOpen(false);
   };
+  const [isChestModalOpen, setIsChestModalOpen] = useState<boolean>(false);
+  const handleMouseChestClick = () => {
+    setIsChestModalOpen(true);
+  };
+  const handleCloseChestModal = () => {
+    setIsChestModalOpen(false);
+  };
   return (
     <div className="Boost max-w-full text-white h-[75vh] max-sm:h-[82vh] mt-12">
       <ToastContainer/>
@@ -78,6 +85,16 @@ export default function Boost() {
           onClick={handleMouseClick}
         >
           <img src="/image/icon/lightning.svg" alt="" className="w-10 h-10" />
+          <div className="flex flex-col">
+            <h3 className="text-2xl text-white">Chest</h3>
+            <h3 className="text-xl text-[#a8a8a7]">3/3 available</h3>
+          </div>
+        </div>
+        <div
+          className={`flex my-3 px-5 py-3 items-center bg-[#4d4d4c] rounded-[30px] hover:bg-[#3a3a3a]`}
+          onClick={handleMouseChestClick}
+        >
+          <img src="/image/treasure.png" alt="" className="w-10 h-10" />
           <div className="flex flex-col">
             <h3 className="text-2xl text-white">Full energy</h3>
             <h3 className="text-xl text-[#a8a8a7]">6/6 available</h3>
@@ -123,6 +140,18 @@ export default function Boost() {
             <h1 className="text-white text-2xl">FREE</h1>
           </div>
           <div className="w-full h-9 bg-indigo-600 text-white rounded-[20px] flex justify-center items-center" onClick={handleFullEnergy}><span className="flex justify-center items-center">Go ahead</span></div>
+        </div>
+      </Modal>
+      <Modal isOpen={isChestModalOpen} onClose={handleCloseChestModal}>
+        <div className="flex flex-col items-center align-middle gap-3">
+          <img src="image/treasure.png" alt="" className=" w-12 h-12"/>
+          <h1 className="text-2xl text-white">Chest</h1>
+          <p className=" text-sm text-white">You can receive the Chest Coins +1000</p>
+          <div className="flex items-center">
+            <img src="image/dollar.png" alt="" className=" w-14 h-14"/>
+            <h1 className="text-white text-2xl">FREE</h1>
+          </div>
+          <div className="w-full h-9 bg-indigo-600 text-white rounded-[20px] flex justify-center items-center" onClick={handleMultiTap}><span className="flex justify-center items-center">Go ahead</span></div>
         </div>
       </Modal>
       <Modal isOpen={isTapModalOpen} onClose={handleCloseTapModal}>
