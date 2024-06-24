f7651dedcbb67dbb3857a2c2c15e31bd07741279
Senior Coder <agilecoder2040@gmail.com>
2024-06-24 14:20:16 +0900

    d

--- src/component/Loading.tsx
+++ src/component/Loading.tsx
@@ -1,14 +1,13 @@
-import "./Loading.css"
+import "./Loading.css";
 const Loading = () => {
-    return (
-        <div className="loadingspinner">
-            <div id="square1" style={{backgroundImage: "url('image/mike_1.png')", backgroundSize: "cover"}}></div>
-            <div id="square2" style={{backgroundImage: "url('image/mike_2.png')", backgroundSize: "cover"}}></div>
-            <div id="square3" style={{backgroundImage: "url('image/mike_3.png')", backgroundSize: "cover"}}></div>
-            <div id="square4" style={{backgroundImage: "url('image/mike_4.png')", backgroundSize: "cover"}}></div>
-            <div id="square5" style={{backgroundImage: "url('image/mike_5.png')", backgroundSize: "cover"}}></div>
-        </div>
-    );
+  return (
+    <div className="loader">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
-export default Loading
+export default Loading;
