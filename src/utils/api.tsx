05d83690e7a98d94f9cda47de0d8dd032ff836cb
Senior Coder <agilecoder2040@gmail.com>
2024-06-30 00:30:01 +0900

    d

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: `http://localhost:5000/api`,
+    baseURL: `https://darrel-clicker-game-backend-1.onrender.com/api`,
     headers: {
       'Content-Type': 'application/json'
     }
